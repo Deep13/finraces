2ba900872d0bc029d5fc8437a11d998a9d6dafdb
mohit1607 <mohit.ashliya@gmail.com>
2024-10-30 14:06:58 +0530

    change

--- src/Sections/Homepage/Heading.jsx
+++ src/Sections/Homepage/Heading.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import mobile from '../../assets/images/mocupmobile.png'
 
 const Heading = () => {
     return (
         <div className='w-full relative mb-[3.29rem]'>
             <h2 className='text-[2.14rem] text-center font-bold mb-[2.06rem]'>Heading</h2>
+            <div className='w-full'>
+                <div></div>
+                <div>
+                    <img src={mobile} alt="exaampler mobile image" />
+                </div>
+            </div>
         </div>
     )
 }
