72e1661532ba6b233ff55d1e1ba3050e0da8a5a4
mohit1607 <mohit.ashliya@gmail.com>
2024-11-04 16:18:35 +0530

    added the icons for social media in footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import shape from '../assets/images/Footer/shape.svg' 
+import shape from '../assets/images/Footer/shape.svg'
+import fb from '../assets/images/fb.svg'
+import g from '../assets/images/g.svg'
+import Youtube from '../assets/images/Youtube.svg'
+import insta from '../assets/images/insta.svg'
+import whatsapp from '../assets/images/whatsapp.svg'
+import telegram from '../assets/images/telegram.svg'
+
 
 
 const Footer = () => {
@@ -10,75 +17,81 @@ const Footer = () => {
       </div>
 
       {/* first column  */}
-        <div className='w-[34.23rem] h-full relative'>
-            <h5 className='text-[0.5rem] mb-[1rem]'>Navigation</h5>
-            <div className='flex w-full gap-[5.64rem]'>
-              <div className='flex flex-col gap-[6px]'>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-              </div>
-              <div className='flex flex-col gap-[6px]'>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-                <p className='text-[0.82rem]'>Lorem, ipsum</p>
-              </div>
-            </div>
-            {/* copyrights  */}
-            <div className='absolute left-0 bottom-0 flex flex-col gap-[5px]'>
-              <p className='text-[0.7rem]'>Copyright</p>
-              <p className='text-[0.7rem]'>Privacy</p>
-              <p className='text-[0.7rem]'>All rights reserved</p>
-            </div>
+      <div className='w-[34.23rem] h-full relative'>
+        <h5 className='text-[0.5rem] mb-[1rem]'>Navigation</h5>
+        <div className='flex w-full gap-[5.64rem]'>
+          <div className='flex flex-col gap-[6px]'>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+          </div>
+          <div className='flex flex-col gap-[6px]'>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+            <p className='text-[0.82rem]'>Lorem, ipsum</p>
+          </div>
+        </div>
+        {/* copyrights  */}
+        <div className='absolute left-0 bottom-0 flex flex-col gap-[5px]'>
+          <p className='text-[0.7rem]'>Copyright</p>
+          <p className='text-[0.7rem]'>Privacy</p>
+          <p className='text-[0.7rem]'>All rights reserved</p>
+        </div>
+      </div>
+
+      {/* second column  */}
+      <div className='w-[34rem] h-full flex flex-col gap-[3.77rem] relative'>
+        <h5 className='text-[0.5rem] mb-[1rem]'>Contact us</h5>
+        <div className='flex w-full gap-[5.64rem]'>
+          <div className='flex flex-col gap-[6px]'>
+            <p className='text-[0.82rem]'>+1 (406) 555-0120</p>
+            <p className='text-[0.82rem]'>+1 (406) 555-0120</p>
+          </div>
+          <div className='flex flex-col gap-[6px]'>
+            <p className='text-[0.82rem]'>hello@logoipsum.com</p>
+          </div>
         </div>
 
-        {/* second column  */}
-        <div className='w-[34rem] h-full flex flex-col gap-[3.77rem] relative'>
-            <h5 className='text-[0.5rem] mb-[1rem]'>Contact us</h5>
-            <div className='flex w-full gap-[5.64rem]'>
-              <div className='flex flex-col gap-[6px]'>
-                <p className='text-[0.82rem]'>+1 (406) 555-0120</p>
-                <p className='text-[0.82rem]'>+1 (406) 555-0120</p>
-              </div>
-              <div className='flex flex-col gap-[6px]'>
-                <p className='text-[0.82rem]'>hello@logoipsum.com</p>
-              </div>
+        {/* 64 px GAP HERE  */}
+
+        <div className='flex w-full gap-[5.64rem]'>
+          <div className='flex flex-col gap-[6px]'>
+            <h5 className='text-[0.5rem] mb-[1.4rem]'>follow us</h5>
+            <div className='w-full flex gap-[10px]'>
+              <img src={fb} alt="" />
+              <img src={g} alt="" />
+              <img src={insta} alt="" />
+              <img src={Youtube} alt="" />
             </div>
-
-            {/* 64 px GAP HERE  */}
-
-            <div className='flex w-full gap-[5.64rem]'>
-              <div className='flex flex-col gap-[6px]'>
-                <h5 className='text-[0.5rem] mb-[1.4rem]'>follow us</h5>
-                <div>
-
-                </div>
-              </div>
-              <div className='flex flex-col gap-[6px]'>
-                <h5 className='text-[0.5rem] mb-[1.4rem] text-center'>Lets chat</h5>
-                <p className='text-[0.82rem]'>hello@logoipsum.com</p>
-              </div>
+          </div>
+          <div className='flex flex-col gap-[6px]'>
+            <h5 className='text-[0.5rem] mb-[1.4rem] text-center'>Lets chat</h5>
+            <div className='flex gap-[10px]'>
+              <img src={telegram} alt="" />
+              <img src={whatsapp} alt="" />
             </div>
+          </div>
+        </div>
 
-            {/* 64 px GAP HERE  */}
+        {/* 64 px GAP HERE  */}
 
-            <div className='flex w-full gap-[5.64rem]'>
-              <div className='flex flex-col gap-[6px]'>
-                <h5 className='text-[0.5rem] mb-[1.4rem]'>Location</h5>
-                <div className='text-[0.82rem]'>
-                  2259 Westheimer Rd. Santa Ana, Illinois 85486 
-                </div>
-              </div>
+        <div className='flex w-full gap-[5.64rem]'>
+          <div className='flex flex-col gap-[6px]'>
+            <h5 className='text-[0.5rem] mb-[1.4rem]'>Location</h5>
+            <div className='text-[0.82rem]'>
+              2259 Westheimer Rd. Santa Ana, Illinois 85486
             </div>
+          </div>
+        </div>
 
-            {/* copyrights  */}
-            <div className='absolute left-0 bottom-0 flex flex-col gap-[5px]'>
+        {/* copyrights  */}
+        <div className='absolute left-0 bottom-0 flex flex-col gap-[5px]'>
           <p className='text-[0.7rem]'>© 2024 — Logoipsum</p>
-            </div>
         </div>
+      </div>
 
     </footer>
   )
