04b2b9abcd9f1f1a6f48f45539a0b1f94295d7c5
mohit1607 <mohit.ashliya@gmail.com>
2024-11-05 12:44:29 +0530

    footer fixed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,14 +11,14 @@ import telegram from '../assets/images/telegram.svg'
 
 const Footer = () => {
   return (
-    <footer className='w-full h-[37.05rem] bg-[#e5f4ff] flex gap-[1.9rem] justify-center items-center pt-[2.8rem] relative'>
+    <footer className='w-full bg-[#e5f4ff] gap-[1.9rem] grid grid-cols-1 md:grid-cols-2 pt-[2.8rem] px-[5%] relative'>
       <div className='absolute bottom-0 left-0 w-full'>
         <img className='w-[34.23rem]' src={shape} alt="shape" />
       </div>
 
       {/* first column  */}
       <div className='w-[34.23rem] h-full relative'>
-        <h5 className='text-[0.5rem] mb-[1rem]'>Navigation</h5>
+        <h5 className='text-[1rem] mb-[1rem] font-semibold'>Navigation</h5>
         <div className='flex w-full gap-[5.64rem]'>
           <div className='flex flex-col gap-[6px]'>
             <p className='text-[0.82rem]'>Lorem, ipsum</p>
@@ -35,16 +35,16 @@ const Footer = () => {
           </div>
         </div>
         {/* copyrights  */}
-        <div className='absolute left-0 bottom-0 flex flex-col gap-[5px]'>
-          <p className='text-[0.7rem]'>Copyright</p>
-          <p className='text-[0.7rem]'>Privacy</p>
-          <p className='text-[0.7rem]'>All rights reserved</p>
-        </div>
+        {/* <div className='absolute left-0 bottom-0 flex flex-col gap-[5px]'>
+          <p className='text-[0.9rem]'>Copyright</p>
+          <p className='text-[0.9rem]'>Privacy</p>
+          <p className='text-[0.9rem]'>All rights reserved</p>
+        </div> */}
       </div>
 
       {/* second column  */}
-      <div className='w-[34rem] h-full flex flex-col gap-[3.77rem] relative'>
-        <h5 className='text-[0.5rem] mb-[1rem]'>Contact us</h5>
+      <div className='w-[34rem] h-full flex flex-col gap-[3.77rem] relative pb-[3rem]'>
+        <h5 className='text-[1rem] mb-[1rem] font-semibold'>Contact us</h5>
         <div className='flex w-full gap-[5.64rem]'>
           <div className='flex flex-col gap-[6px]'>
             <p className='text-[0.82rem]'>+1 (406) 555-0120</p>
@@ -57,9 +57,9 @@ const Footer = () => {
 
         {/* 64 px GAP HERE  */}
 
-        <div className='flex w-full gap-[5.64rem]'>
+        <div className='w-full gap-[5.64rem] grid grid-cols-1 lg:grid-cols-2'>
           <div className='flex flex-col gap-[6px]'>
-            <h5 className='text-[0.5rem] mb-[1.4rem]'>follow us</h5>
+            <h5 className='text-[1rem] mb-[1.4rem] font-semibold'>follow us</h5>
             <div className='w-full flex gap-[10px]'>
               <img src={fb} alt="" />
               <img src={g} alt="" />
@@ -68,7 +68,7 @@ const Footer = () => {
             </div>
           </div>
           <div className='flex flex-col gap-[6px]'>
-            <h5 className='text-[0.5rem] mb-[1.4rem] text-center'>Lets chat</h5>
+            <h5 className='text-[1rem] mb-[1.4rem] text-start font-semibold'>Lets chat</h5>
             <div className='flex gap-[10px]'>
               <img src={telegram} alt="" />
               <img src={whatsapp} alt="" />
@@ -80,7 +80,7 @@ const Footer = () => {
 
         <div className='flex w-full gap-[5.64rem]'>
           <div className='flex flex-col gap-[6px]'>
-            <h5 className='text-[0.5rem] mb-[1.4rem]'>Location</h5>
+            <h5 className='text-[1rem] mb-[1.4rem] font-semibold'>Location</h5>
             <div className='text-[0.82rem]'>
               2259 Westheimer Rd. Santa Ana, Illinois 85486
             </div>
@@ -89,7 +89,7 @@ const Footer = () => {
 
         {/* copyrights  */}
         <div className='absolute left-0 bottom-0 flex flex-col gap-[5px]'>
-          <p className='text-[0.7rem]'>© 2024 — Logoipsum</p>
+          <p className='text-[0.9rem]'>© 2024 — Logoipsum</p>
         </div>
       </div>
 
