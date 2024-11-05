e6105c437fce3c909c74821613083d1a90aa7b5c
mohit1607 <mohit.ashliya@gmail.com>
2024-11-05 15:34:37 +0530

    Layout fixed for sidebar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { IoIosAdd } from "react-icons/io"; 
 import { HiMenu } from "react-icons/hi";
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
@@ -21,36 +22,6 @@ const Navbar = () => {
 
     return (
         <>
-            <div className={`${toggle ? '-translate-x-[10rem]' : 'translate-x-0'} fixed transition-transform ease-out duration-300 left-[0.8rem] top-[6.5rem] flex flex-col gap-[0.7rem] z-[10]`}>
-                <button className="w-[3.35rem] h-[4.1rem] rounded-[10px] bg-[#e5f4ff] gap-[5px] text-[0.6rem] font-bold flex flex-col justify-center items-center">
-                    <img src={compass} alt="discover" />
-                    Discover
-                </button>
-                <button className="w-[3.35rem] h-[4.1rem] rounded-[10px] bg-[#e5f4ff] gap-[5px] text-[0.6rem] font-bold flex flex-col justify-center items-center">
-                    <img src={stats} alt="discover" />
-                    Stocks
-                </button>
-                <button className="w-[3.35rem] h-[4.1rem] rounded-[10px] bg-[#e5f4ff] gap-[5px] text-[0.6rem] font-bold flex flex-col justify-center items-center">
-                    <img src={eth} alt="discover" />
-                    Crypto
-                </button>
-                <button className="w-[3.35rem] h-[4.1rem] rounded-[10px] bg-[#e5f4ff] gap-[5px] text-[0.6rem] font-bold flex flex-col justify-center items-center">
-                    <img src={live_streaming} alt="discover" />
-                    Live Races
-                </button>
-                <button className="w-[3.35rem] h-[4.1rem] rounded-[10px] bg-[#e5f4ff] gap-[5px] text-[0.6rem] font-bold flex flex-col justify-center items-center">
-                    <img src={recent} alt="discover" />
-                    Upcoming Races
-                </button>
-                <button className="w-[3.35rem] h-[4.1rem] rounded-[10px] bg-[#e5f4ff] gap-[5px] text-[0.6rem] font-bold flex flex-col justify-center items-center">
-                    <img src={finance_idea} alt="discover" />
-                    My Races
-                </button>
-                <button className="w-[3.35rem] h-[4.1rem] rounded-[10px] bg-[#e5f4ff] gap-[5px] text-[0.6rem] font-bold flex flex-col justify-center items-center">
-                    <img src={forward} alt="discover" />
-                    My Watchlist
-                </button>
-            </div>
             <nav className='w-full px-[1.5rem] py-[0.8rem] bg-[#e5f4ff] flex items-center justify-between sticky top-0 z-[10]'>
                 <div className="flex items-center">
                     {/* <button onClick={() => setToggle(prev => !prev)} className='w-[2.9rem] h-[2.9rem] grid place-items-center rounded-[8px] mr-[106px]'>
@@ -69,12 +40,20 @@ const Navbar = () => {
                     <button onClick={() => navigate('/auth')} className="bg-[#e4eaf0] px-[1.5rem] h-[2.35rem] text-[0.9rem] rounded-[8px] grid place-items-center text-black font-semibold">
                         Log in / Sign up
                     </button> : 
+                    <>
                     <button onClick={() =>{
                         localStorage.removeItem('token')
                         navigate('/auth')
                     }} className="bg-[#e4eaf0] px-[1.5rem] h-[2.35rem] text-[0.9rem] rounded-[8px] grid place-items-center text-black font-semibold">
                         Log out
                     </button>
+                    <button onClick={() => {
+                        // create Race
+                    }} className="bg-[#e4eaf0] pl-[1.5rem] pr-[0.8rem] h-[2.35rem] text-[0.9rem] rounded-[8px] flex gap-2 items-center text-black font-semibold">
+                        Create Race
+                        <IoIosAdd size={20}/>
+                    </button>
+                    </>
                     }
                     <button className='aspect-square h-[2.35rem] grid place-items-center bg-white rounded-[8px]'>
                         <img src={support} alt="Search" />
